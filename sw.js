e74d0ebc195db27736934427d38984c7eabdcb67
cheralynli <cheralynlianto@gmail.com>
2026-06-13 09:18:49 +0800

    Make egg timer installable as PWA

---
+++ sw.js
@@ -0,0 +1,63 @@
+const CACHE_NAME = "always-perfect-eggs-v6";
+const APP_ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./egg-1.png",
+  "./egg-2.png",
+  "./egg-3.png",
+  "./egg-4.png",
+  "./egg-5.png",
+  "./icon-180.png",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  const requestUrl = new URL(event.request.url);
+  const isAppPage = event.request.mode === "navigate" || requestUrl.pathname.endsWith("/index.html");
+
+  if (isAppPage) {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const responseCopy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseCopy));
+          return response;
+        })
+        .catch(() => caches.match(event.request).then((cachedResponse) => {
+          return cachedResponse || caches.match("./index.html");
+        }))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
